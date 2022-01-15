05d73a8cd89ad7732c599116de3f8655df0ddcf1
BadTonne <maxim.ev.kiva@gmail.com>
2022-01-15 18:03:04 +0300

    Create MultiApartmentFlat.cpp

---
+++ MultiApartmentFlat.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string apartment[10];
+
+    for (int i = 1; i < 11; i++)
+    {
+        cout << "Input tenant's name in apartment #" << i << ":" << endl;
+        cin >> apartment[i - 1];
+        cout << endl;
+    }
+
+    int current;
+    cout << endl;
+    
+    while (true)
+    {
+        cout << endl << "Input apartment number:" << endl;
+        cin >> current;
+        
+        if (current > 10 || current < 1)
+        {
+            cout << "Invalid input. Please try again." << endl;
+        }
+        else
+        {
+            current--;
+            cout << "Here's living " << apartment[current] << endl;
+        }
+    }
+}
